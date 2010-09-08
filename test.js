7af9e745e8cff0dcc6e50946d5431b17f89606b0
Mattias Zippert <mattias@zippert.org>
2010-09-08 13:25:37 +0200

    Changed purpose to logon screen including SHA-1 encryption.

--- test.js
+++ test.js
@@ -1,17 +1,11 @@
-var startTime = new Date;
+var allowedHash = new Array("a9993e364706816aba3e25717850c26c9cd0d89d");
 
-function startTimer() {
-	var date = new Date();
-	var diffHour = date.getHours() - startTime.getHours();
-	var diffMinute = date.getMinutes() - startTime.getMinutes();
-	var diffSeconds = date.getSeconds() - startTime.getSeconds();
-	var secDiff = diffSeconds + diffMinute * 60 + diffHour * 3600;
-	if(secDiff > 59){
-		var sec = secDiff%60;
-		document.getElementById('main').innerHTML = diffMinute + " minutes and " + sec + " seconds";
-	} else{
-		document.getElementById('main').innerHTML = secDiff + " seconds";	
+function validate_form(form) {
+	var searchFor = sha1Hash(form.login.value + form.password.value);
+	var foundMatch = false;
+	for(var i = 0; i < allowedHash.length && !foundMatch; i++){
+		foundMatch = (allowedHash[i] == searchFor);
 	}
-	
-	setTimeout('startTimer()', 500);
+	alert(foundMatch ? "Valid" : "Not valid");
+	return false;
 }
